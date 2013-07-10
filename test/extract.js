a6762ae75f4b66170bf3e80efe4ff7706e7eb89e
Toby Miller <tobius.miller@gmail.com>
2013-07-10 16:35:59 -0400

    improved unit test

--- test/extract.js
+++ test/extract.js
@@ -3,18 +3,22 @@ var colormatch = require(__dirname+'/../main.js'),
 
 colormatch.extract(__dirname + '/flower.jpg', function(err, data){
 
+    // imagemagick error
     if (err){
-        console.log(err);
+        throw new Error(err);
         process.exit(1);
     }
 
-    console.log('data', data);
-
+    // run tests
     assert.ok(data.length === 24);
     assert.ok(data[0].hex);
     assert.ok(data[0].percent);
     assert.ok(data[0].pixels);
     assert.ok(data[0].rgb);
+
+    // tests passed
+    console.log('ok');
+    process.exit(0);
 
 });
 
