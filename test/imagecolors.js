8f092d78d5df17a36a674e207ac60eefd6aee218
Toby Miller <tobius.miller@gmail.com>
2013-11-20 11:14:07 -0500

    much improved unit tests

--- test/imagecolors.js
+++ test/imagecolors.js
@@ -1,56 +1,190 @@
-var imagecolors = require(__dirname+'/../main.js'),
-    assert = require('assert');
 
-var assets = [__dirname + '/octocat.png', 'http://octodex.github.com/images/original.png'];
+var assert, chai, imagecolors, testcolors;
 
-assets.forEach(function(asset){
+// modules
+chai        = require('chai');
+imagecolors = require(__dirname + '/../main.js');
 
-    imagecolors.extract(asset, 8, function(err, colors){
+// enable stack traces
+chai.Assertion.includeStack = true;
 
-        // extraction error
-        if (err){
-            throw new Error(err);
-            process.exit(1);
-        }
+// use chai assert
+assert = chai.assert;
 
-        // run tests
-        assert.ok(colors.length === 8);
-        assert.ok(colors[0].pixels);
-        assert.ok(colors[0].hex);
-        assert.ok(colors[0].labelHex);
-        assert.ok(colors[0].rgb);
-        assert.ok(colors[0].hsl);
-        assert.ok(colors[0].hsv);
-        assert.ok(colors[0].cmyk);
-        assert.ok(colors[0].luminance);
-        assert.ok(colors[0].percent);
-        assert.ok(colors[0].family);
+describe('require("imagecolors")', function(){
 
-        // test convert
-        imagecolors.convert(colors, __dirname + '/palette.json', function(err, customColors){
+    it('should be a valid object', function(){
+        assert(imagecolors !== undefined, 'must be defined');
+        assert(typeof imagecolors === 'object', 'must be an object');
+    });
 
-            // conversion error
-            if (err){
-                throw new Error(err);
-                process.exit(1);
-            }
+    describe('imagecolors.extract()', function(){
 
-            // run tests
-            assert.ok(colors.length === 8);
-            assert.ok(colors[0].pixels);
-            assert.ok(colors[0].hex);
-            assert.ok(colors[0].labelHex);
-            assert.ok(colors[0].rgb);
-            assert.ok(colors[0].hsl);
-            assert.ok(colors[0].hsv);
-            assert.ok(colors[0].cmyk);
-            assert.ok(colors[0].luminance);
-            assert.ok(colors[0].percent);
-            assert.ok(colors[0].family);
-
-            // tests passed
-            console.log('ok');
+        it('should be a valid method', function(){
+            assert(imagecolors.extract !== undefined, 'must be defined');
+            assert(typeof imagecolors.extract === 'function', 'must be a function');
         });
+
+        it('should extract valid color properties from a local image', function(done){
+
+            var asset = __dirname + '/octocat.png';
+
+            imagecolors.extract(asset, 8, function(err, colors){
+
+                // extraction error
+                assert.notOk(err);
+
+                // run tests
+                assert.ok(colors.length === 8);
+                assert.ok(colors[0].pixels);
+                assert.ok(colors[0].hex);
+                assert.ok(colors[0].labelHex);
+                assert.ok(colors[0].rgb);
+                assert.ok(colors[0].hsl);
+                assert.ok(colors[0].hsv);
+                assert.ok(colors[0].cmyk);
+                assert.ok(colors[0].luminance);
+                assert.ok(colors[0].percent);
+                assert.ok(colors[0].family);
+
+                // continue testing
+                done();
+
+            });
+
+        });
+
+        it('should return error when extracting color properties from a local non-image', function(done){
+
+            var asset = __dirname + '/lorem.txt';
+
+            imagecolors.extract(asset, 8, function(err, colors){
+
+                // extraction error
+                assert.ok(err);
+
+                // continue testing
+                done();
+
+            });
+
+        });
+
+        it('should extract valid color properties from a remote image', function(done){
+
+            var asset = 'http://octodex.github.com/images/original.png';
+
+            imagecolors.extract(asset, 8, function(err, colors){
+
+                // extraction error
+                assert.notOk(err);
+
+                // run tests
+                assert.ok(colors.length === 8);
+                assert.ok(colors[0].pixels);
+                assert.ok(colors[0].hex);
+                assert.ok(colors[0].labelHex);
+                assert.ok(colors[0].rgb);
+                assert.ok(colors[0].hsl);
+                assert.ok(colors[0].hsv);
+                assert.ok(colors[0].cmyk);
+                assert.ok(colors[0].luminance);
+                assert.ok(colors[0].percent);
+                assert.ok(colors[0].family);
+
+                // continue testing
+                done();
+
+            });
+
+        });
+
+        it('should return error when extracting color properties from a remote non-image', function(done){
+
+            var asset = 'http://octodex.github.com/';
+
+            imagecolors.extract(asset, 8, function(err, colors){
+
+                // extraction error
+                assert.ok(err);
+
+                // continue testing
+                done();
+
+            });
+
+        });
+
+    });
+
+    describe('imagecolors.convert()', function(){
+
+        beforeEach(function(done){
+
+            var asset = __dirname + '/octocat.png';
+
+            imagecolors.extract(asset, 8, function(err, colors){
+
+                // set test colors
+                testcolors = colors;
+
+                // continue testing
+                done();
+
+            });
+
+        });
+
+        it('should be a valid method', function(){
+            assert(imagecolors.convert !== undefined, 'must be defined');
+            assert(typeof imagecolors.convert === 'function', 'must be a function');
+        });
+
+        it('should convert valid color properties from a valid json color palette', function(done){
+
+            var palette = __dirname + '/palette.json';
+
+            imagecolors.convert(testcolors, palette, function(err, colors){
+
+                // extraction error
+                assert.notOk(err);
+
+                // run tests
+                assert.ok(colors.length === 8);
+                assert.ok(colors[0].pixels);
+                assert.ok(colors[0].hex);
+                assert.ok(colors[0].labelHex);
+                assert.ok(colors[0].rgb);
+                assert.ok(colors[0].hsl);
+                assert.ok(colors[0].hsv);
+                assert.ok(colors[0].cmyk);
+                assert.ok(colors[0].luminance);
+                assert.ok(colors[0].percent);
+                assert.ok(colors[0].family);
+
+                // continue testing
+                done();
+
+            });
+
+        });
+
+        it('should return error when converting color properties from an invalid json color palette', function(done){
+
+            var palette = __dirname + '/lorem.txt';
+
+            imagecolors.convert(testcolors, palette, function(err, colors){
+
+                // extraction error
+                assert.ok(err);
+
+                // continue testing
+                done();
+
+            });
+
+        });
+
     });
 
 });
