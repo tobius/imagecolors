0d4df61a9f18b7e5d9c65c35bab9b933749cb8f2
Toby Miller <tobius.miller@gmail.com>
2013-11-20 16:54:56 -0500

    updated examples to work as a local web instance

---
+++ examples/README.md
@@ -0,0 +1,11 @@
+
+# Examples
+
+This folder contains some examples of the imagecolors module in use.
+
+# Run
+
+    [~] npm install
+    [~] foreman start
+    [~] open http://localhost:5000
+
---
+++ examples/web.js
@@ -0,0 +1,80 @@
+
+// express
+var _           = require('underscore'),
+    express     = require('express'),
+    fs          = require('fs'),
+    imagecolors = require('imagecolors'),
+    app         = express();
+
+// template
+var head = fs.readFileSync(__dirname + '/templates/head.tmpl', 'utf8');
+    foot = fs.readFileSync(__dirname + '/templates/foot.tmpl', 'utf8');
+
+// configure
+app.configure(function(){
+
+    // index
+    app.get('/', function(req, res){
+
+        var i, files, menu, photos;
+
+        // find photos
+        photos = [];
+        files = fs.readdirSync(__dirname + '/public/photos/');
+        for(i = files.length - 1; i > -1; i--){
+            if (/\.(gif|jpg|jpeg|png)$/i.test(files[i])){
+                photos.push(files[i]);
+            }
+        }
+
+        // generate menu
+        menu = '';
+        for(i = photos.length - 1; i > -1; i--){
+            menu += '<li><a href="/example.html?photo=' + photos[i] + '">' + photos[i] + '</a></li>';
+        }
+
+        // output
+        res.send(head + '<strong>Examples</strong><ul>' + menu + '</ul>' + foot);
+
+    });
+
+    // examples
+    app.get('/example.html', function(req, res){
+
+        var photo = req.query.photo || null;
+
+        if (photo !== null){
+
+            // extract image colors
+            imagecolors.extract(__dirname + '/public/photos/' + photo, 5, function(err, colors){
+
+                var body = '<strong>Colors</strong><img src="/photos/' + photo + '" width="380"/><div class="container">';
+                colors.forEach(function(color){
+                    body += '<div class="color" data-family="' + color.family + '" data-luminance="' + color.luminance + '" data-pixels="' + color.pixels + '" data-percent="' + color.percent + '" style="color:' + color.labelHex + ';background-color:' + color.hex + '">';
+                    body += '<strong>' + color.hex + '</strong> ' + color.family + ' family';
+                    body += '</div>';
+                });
+                body += '</div><div style="clear:both; height:30px;"></div>';
+                body += '<strong>Output</strong><div class="json">' + JSON.stringify(colors, null, 4) + '</div>'
+                body += '<div style="clear:both;"></div>';
+
+                res.send(head + body + foot);
+
+            });
+
+        } else {
+
+            res.send('unknown photo');
+
+        }
+
+    });
+
+    // static
+    app.use(express.static(__dirname + '/public'));
+
+});
+
+// http server
+app.listen(5000);
+
