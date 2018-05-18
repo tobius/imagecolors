b17f7b66759e6390315b343963017a6daed53357
Toby Miller <tobius.miller@gmail.com>
2018-05-17 23:55:52 -0400

    Add eslint support
    - add eslint module
    - add .eslintrc file
    - add .eslintignore file
    - add .editorconfig file
    - update source code to satisfy eslint feedback

--- examples/web.js
+++ examples/web.js
@@ -1,77 +1,86 @@
+/*eslint max-nested-callbacks: [0] */
 
 // express
-var _           = require('lodash'),
-    express     = require('express'),
-    fs          = require('fs'),
-    imagecolors = require('imagecolors'),
-    app         = express();
+var express = require('express'),
+	fs = require('fs'),
+	imagecolors = require('imagecolors'),
+	app = express();
 
 // template
 var head = fs.readFileSync(__dirname + '/templates/head.tmpl', 'utf8');
 var foot = fs.readFileSync(__dirname + '/templates/foot.tmpl', 'utf8');
 
 // configure
-app.configure(function(){
+app.configure(function() {
 
-    // index
-    app.get('/', function(req, res){
+	// index
+	app.get('/', function(req, res) {
 
-        var i, files, menu, photos;
+		var i, files, menu, photos;
 
-        // find photos
-        photos = [];
-        files = fs.readdirSync(__dirname + '/public/photos/');
-        for(i = files.length - 1; i > -1; i -= 1){
-            if (/\.(gif|jpg|jpeg|png)$/i.test(files[i])){
-                photos.push(files[i]);
-            }
-        }
+		// find photos
+		photos = [];
+		files = fs.readdirSync(__dirname + '/public/photos/');
+		for(i = files.length - 1; i > -1; i -= 1) {
+			if (/\.(gif|jpg|jpeg|png)$/i.test(files[i])) {
+				photos.push(files[i]);
+			}
+		}
 
-        // generate menu
-        menu = '';
-        for(i = photos.length - 1; i > -1; i -= 1){
-            menu += '<li><a href="/example.html?photo=' + photos[i] + '">' + photos[i] + '</a></li>';
-        }
+		// generate menu
+		menu = '';
+		for(i = photos.length - 1; i > -1; i -= 1) {
+			menu += '<li><a href="/example.html?photo=' + photos[i] + '">' + photos[i] + '</a></li>';
+		}
 
-        // output
-        res.send(head + '<strong>Examples</strong><ul>' + menu + '</ul>' + foot);
+		// output
+		res.send(head + '<strong>Examples</strong><ul>' + menu + '</ul>' + foot);
 
-    });
+	});
 
-    // examples
-    app.get('/example.html', function(req, res){
+	// examples
+	app.get('/example.html', function(req, res) {
 
-        var photo = req.query.photo || null;
+		var photo = req.query.photo || null;
 
-        if (photo !== null){
+		if (photo !== null) {
 
-            // extract image colors
-            imagecolors.extract(__dirname + '/public/photos/' + photo, 5, function(err, colors){
+			// extract image colors
+			imagecolors.extract(__dirname + '/public/photos/' + photo, 5, function(err, colors) {
 
-                var body = '<strong>Colors</strong><img src="/photos/' + photo + '" width="380"/><div class="container">';
-                colors.forEach(function(color){
-                    body += '<div class="color" data-family="' + color.family + '" data-luminance="' + color.luminance + '" data-pixels="' + color.pixels + '" data-percent="' + color.percent + '" style="color:' + color.labelHex + ';background-color:' + color.hex + '">';
-                    body += '<strong>' + color.hex + '</strong> ' + color.family + ' family';
-                    body += '</div>';
-                });
-                body += '</div><div style="clear:both; height:30px;"></div>';
-                body += '<strong>Output</strong><div class="json">' + JSON.stringify(colors, null, 4) + '</div>';
-                body += '<div style="clear:both;"></div>';
+				if (err) {
+					return res.send(err.message);
+				}
 
-                res.send(head + body + foot);
+				var body = `<strong>Colors</strong><img src="/photos/${photo}" width="380"/><div class="container">`;
+				colors.forEach(function(color) {
+					body += '<div class="color" data-family="' + color.family +
+						'" data-luminance="' + color.luminance +
+						'" data-pixels="' + color.pixels +
+						'" data-percent="' + color.percent +
+						'" style="color:' + color.labelHex +
+						';background-color:' + color.hex + '">';
+					body += '<strong>' + color.hex + '</strong> ' + color.family + ' family';
+					body += '</div>';
+				});
+				body += '</div><div style="clear:both; height:30px;"></div>';
+				body += '<strong>Output</strong><div class="json">' + JSON.stringify(colors, null, 4) + '</div>';
+				body += '<div style="clear:both;"></div>';
 
-            });
+				res.send(head + body + foot);
 
-        } else {
+			});
 
-            res.send('unknown photo');
+		} else {
 
-        }
+			res.send('unknown photo');
 
-    });
+		}
 
-    // static
-    app.use(express.static(__dirname + '/public'));
+	});
+
+	// static
+	app.use(express.static(__dirname + '/public'));
 
 });
 
